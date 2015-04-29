4c0832d70f2d4860f28aa4dfe9a1da94320eeb7b
orkenstein <orkru666@gmail.com>
2015-04-29 20:41:25 +0300

    Initial Commit

---
+++ SieveOfEratosthenes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SieveOfEratosthenes
+//
+//  Created by orkenstein on 29.04.15.
+//  Copyright (c) 2015 orkenstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SieveOfEratosthenes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SieveOfEratosthenes
+//
+//  Created by orkenstein on 29.04.15.
+//  Copyright (c) 2015 orkenstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
